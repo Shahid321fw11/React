ca69613be95cf13db2785da5c5df1bd4391d01ed
Shahid321fw11 <shahid_fw11_321@masai.school>
2022-03-03 18:23:49 +0530

    almost complete

--- exercise_tracker_app/exercise_tracker/backend/server.js
+++ exercise_tracker_app/exercise_tracker/backend/server.js
@@ -11,15 +11,13 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri);
-
+mongoose.connect(uri, { useNewUrlParser: true});
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
-
-const exerciseRouter = require('./routes/exercise');
-const userRouter = require('./routes/user');
+const exerciseRouter = require('./routes/exercise.js');
+const userRouter = require('./routes/user.js');
 
 app.use('/exercises', exerciseRouter);
 app.use('/users', userRouter);
--- exercise_tracker_app/exercise_tracker/src/App.js
+++ exercise_tracker_app/exercise_tracker/src/App.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import Navbar from "./components/navbar.component"
-import ExercisesList from "./components/exercises-list.component";
-// import EditExercise from "./components/edit-exercise.component";
-import CreateExercise from "./components/create-exercise.component";
-import CreateUser from "./components/create-user.component";
+import Navbar from "./components/navbar_component"
+import ExercisesList from "./components/exercises_list_component";
+import EditExercise from "./components/edit_exercise_component";
+import CreateExercise from "./components/create_exercise_component";
+import CreateUser from "./components/create_user_component";
 
 function App() {
   return (
     <Router>
       <div className="container">
-        <Navbar />
-        <br />
+      <Navbar />
+      <br />
         <Routes>
           <Route path="/" element={<ExercisesList />} />
-          {/* <Route path="/edit/:id" element={<EditExercise />} /> */}
+          <Route path="/edit/:id" element={<EditExercise />} />
           <Route path="/create" element={<CreateExercise />} />
           <Route path="/user" element={<CreateUser />} />
         </Routes>
